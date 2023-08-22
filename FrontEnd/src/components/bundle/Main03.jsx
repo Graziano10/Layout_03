8ecf410734379502d36a7d272aaa2a1e2949db90
Graziano Corineo <grazianocorineo01@gmail.com>
2023-08-23 00:45:16 +0200

    Ultimate MainContent03 -> Home

--- FrontEnd/src/components/bundle/Main03.jsx
+++ FrontEnd/src/components/bundle/Main03.jsx
@@ -343,13 +343,10 @@ const Main03 = () => {
           </div>
 
           {/* Slide 5*/}
-<div data-carousel-item2>
-          <div className="flex flex-col h-full">
-            <div
-              className="duration-700 ease-in-out flex flex-col justify-center items-center h-full text-lg x:flex-row"
-              
-            >
-              <div className="duration-700 ease-in-out flex flex-col justify-center items-center h-full text-lg">
+          <div data-carousel-item2>
+            <div className="flex flex-col h-full">
+              <div className="duration-700 ease-in-out flex flex-col justify-center items-center h-full text-lg x:flex-row">
+                <div className="duration-700 ease-in-out flex flex-col justify-center items-center h-full text-lg">
                   <div className="h-36 w-36 flex flex-col justify-center items-center bg-green-500 rounded-full">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -370,51 +367,52 @@ const Main03 = () => {
                   </p>
                 </div>
 
-              {/* ---02--- */}
-              <div className="flex flex-col justify-center items-center h-full my-10">
-                <div className="h-36 w-36 flex justify-center items-center bg-green-500 rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="white"
-                    class="w-20 h-20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.798 7.45c.512-.67 1.135-.95 1.702-.95s1.19.28 1.702.95a.75.75 0 001.192-.91C12.637 5.55 11.596 5 10.5 5s-2.137.55-2.894 1.54A5.205 5.205 0 006.83 8H5.75a.75.75 0 000 1.5h.77a6.333 6.333 0 000 1h-.77a.75.75 0 000 1.5h1.08c.183.528.442 1.023.776 1.46.757.99 1.798 1.54 2.894 1.54s2.137-.55 2.894-1.54a.75.75 0 00-1.192-.91c-.512.67-1.135.95-1.702.95s-1.19-.28-1.702-.95a3.505 3.505 0 01-.343-.55h1.795a.75.75 0 000-1.5H8.026a4.835 4.835 0 010-1h2.224a.75.75 0 000-1.5H8.455c.098-.195.212-.38.343-.55z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
+                {/* ---02--- */}
+                <div className="flex flex-col justify-center items-center h-full my-10">
+                  <div className="h-36 w-36 flex justify-center items-center bg-green-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="white"
+                      class="w-20 h-20"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.798 7.45c.512-.67 1.135-.95 1.702-.95s1.19.28 1.702.95a.75.75 0 001.192-.91C12.637 5.55 11.596 5 10.5 5s-2.137.55-2.894 1.54A5.205 5.205 0 006.83 8H5.75a.75.75 0 000 1.5h.77a6.333 6.333 0 000 1h-.77a.75.75 0 000 1.5h1.08c.183.528.442 1.023.776 1.46.757.99 1.798 1.54 2.894 1.54s2.137-.55 2.894-1.54a.75.75 0 00-1.192-.91c-.512.67-1.135.95-1.702.95s-1.19-.28-1.702-.95a3.505 3.505 0 01-.343-.55h1.795a.75.75 0 000-1.5H8.026a4.835 4.835 0 010-1h2.224a.75.75 0 000-1.5H8.455c.098-.195.212-.38.343-.55z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <p className="text-center p-3 font-normal tracking-wide">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing.
+                  </p>
                 </div>
-                <p className="text-center p-3 font-normal tracking-wide">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing.
-                </p>
-              </div>
 
-              {/* ---03--- */}
+                {/* ---03--- */}
 
-              <div className="flex flex-col justify-center items-center h-full">
-                <div className="h-36 w-36 flex justify-center items-center bg-green-500 rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="white"
-                    class="w-20 h-20"
-                  >
-                    <path d="M10.75 10.818v2.614A3.13 3.13 0 0011.888 13c.482-.315.612-.648.612-.875 0-.227-.13-.56-.612-.875a3.13 3.13 0 00-1.138-.432zM8.33 8.62c.053.055.115.11.184.164.208.16.46.284.736.363V6.603a2.45 2.45 0 00-.35.13c-.14.065-.27.143-.386.233-.377.292-.514.627-.514.909 0 .184.058.39.202.592.037.051.08.102.128.152z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-6a.75.75 0 01.75.75v.316a3.78 3.78 0 011.653.713c.426.33.744.74.925 1.2a.75.75 0 01-1.395.55 1.35 1.35 0 00-.447-.563 2.187 2.187 0 00-.736-.363V9.3c.698.093 1.383.32 1.959.696.787.514 1.29 1.27 1.29 2.13 0 .86-.504 1.616-1.29 2.13-.576.377-1.261.603-1.96.696v.299a.75.75 0 11-1.5 0v-.3c-.697-.092-1.382-.318-1.958-.695-.482-.315-.857-.717-1.078-1.188a.75.75 0 111.359-.636c.08.173.245.376.54.569.313.205.706.353 1.138.432v-2.748a3.782 3.782 0 01-1.653-.713C6.9 9.433 6.5 8.681 6.5 7.875c0-.805.4-1.558 1.097-2.096a3.78 3.78 0 011.653-.713V4.75A.75.75 0 0110 4z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
+                <div className="flex flex-col justify-center items-center h-full">
+                  <div className="h-36 w-36 flex justify-center items-center bg-green-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="white"
+                      class="w-20 h-20"
+                    >
+                      <path d="M10.75 10.818v2.614A3.13 3.13 0 0011.888 13c.482-.315.612-.648.612-.875 0-.227-.13-.56-.612-.875a3.13 3.13 0 00-1.138-.432zM8.33 8.62c.053.055.115.11.184.164.208.16.46.284.736.363V6.603a2.45 2.45 0 00-.35.13c-.14.065-.27.143-.386.233-.377.292-.514.627-.514.909 0 .184.058.39.202.592.037.051.08.102.128.152z" />
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-6a.75.75 0 01.75.75v.316a3.78 3.78 0 011.653.713c.426.33.744.74.925 1.2a.75.75 0 01-1.395.55 1.35 1.35 0 00-.447-.563 2.187 2.187 0 00-.736-.363V9.3c.698.093 1.383.32 1.959.696.787.514 1.29 1.27 1.29 2.13 0 .86-.504 1.616-1.29 2.13-.576.377-1.261.603-1.96.696v.299a.75.75 0 11-1.5 0v-.3c-.697-.092-1.382-.318-1.958-.695-.482-.315-.857-.717-1.078-1.188a.75.75 0 111.359-.636c.08.173.245.376.54.569.313.205.706.353 1.138.432v-2.748a3.782 3.782 0 01-1.653-.713C6.9 9.433 6.5 8.681 6.5 7.875c0-.805.4-1.558 1.097-2.096a3.78 3.78 0 011.653-.713V4.75A.75.75 0 0110 4z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <p className="text-center p-3 font-normal tracking-wide">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    In!
+                  </p>
                 </div>
-                <p className="text-center p-3 font-normal tracking-wide">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. In!
-                </p>
               </div>
             </div>
-          </div>
           </div>
 
           {/* ... Repeat similar structure for other slides ... */}
