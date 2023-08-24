d2315cdd3218e3ee9afed2d4590e2c020427f425
Graziano Corineo <grazianocorineo01@gmail.com>
2023-08-24 18:00:21 +0200

    Ultimate Footer -> Home

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/bundle/Footer";
 import Header from "../components/bundle/Header";
 import Main from "../components/bundle/Main";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Header />
       <Main />
+      <Footer/>
     </>
   );
 };
